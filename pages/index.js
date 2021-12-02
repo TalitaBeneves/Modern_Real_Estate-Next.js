711961f614c5581e7e52fbcddfb3d2f94e0b55ab
Talita Beneves <talitabeneves15@gmail.com>
2021-12-02 12:44:05 -0300

    Real Estate Properties

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
+import Property from '../components/Property';
 import { baseUrl, fetchApi } from '../utils/fetchApi';
 
 export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
@@ -32,7 +33,7 @@ const Home = ({ propertiesForSale, propertiesForRent }) => (
       imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
     />
      <Flex flexWrap='wrap'>
-      {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
+      {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
     </Flex>
     <Banner
       purpose='BUY A HOME'
